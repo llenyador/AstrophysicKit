5d17845150770211dc4b4e29629dee05a63733bb
Aitor Salvador <aitor.salvador@promocionesfarma.com>
2020-07-22 01:28:18 +0200

    Creates project

---
+++ AstrophysicKit/AstrophysicKit/AstrophysicKit.h
@@ -0,0 +1,19 @@
+//
+//  AstrophysicKit.h
+//  AstrophysicKit
+//
+//  Created by Aitor Salvador García on 22/07/2020.
+//  Copyright © 2020 Aitor Salvador García. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AstrophysicKit.
+FOUNDATION_EXPORT double AstrophysicKitVersionNumber;
+
+//! Project version string for AstrophysicKit.
+FOUNDATION_EXPORT const unsigned char AstrophysicKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AstrophysicKit/PublicHeader.h>
+
+
